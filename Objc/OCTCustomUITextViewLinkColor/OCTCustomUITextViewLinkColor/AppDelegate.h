1eb0f2c55b76295d2a768cc6677b83c109fc3c02
Dmytro Brovkin <dmytro.brovkin@playtech.com>
2017-04-18 15:02:51 +0300

    Initial commit

---
+++ Objc/OCTCustomUITextViewLinkColor/OCTCustomUITextViewLinkColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCTCustomUITextViewLinkColor
+//
+//  Created by dmitry.brovkin on 4/18/17.
+//  Copyright © 2017 dmitry.brovkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
